9edfc70419da69dff017902ba836bcd44e0981c5
XiZhang-Truth <117990019+XiZhang-Truth@users.noreply.github.com>
2022-11-11 21:26:55 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 export default {
-  title: 'yun yu yuan', // 网站标题
-  nickName: "云与原", // 昵称
-  githubName: 'yunyuyuan', // github账户名
+  title: '西漳真心话 XiZhang Truth', // 网站标题
+  nickName: "XiZhang-Truth", // 昵称
+  githubName: 'XiZhang-Truth', // github账户名
   githubRepo: 'nuxt2-blog', // 勿修改
   githubBranch: 'master', // 勿修改
-  domain: 'https://blog.halberd.cn', // rss域名
-  SEO_description: '云与原的博客，书写值得书写的东西',
-  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,云与原的博客',
+  domain: 'https://www.xzzxh.tk', // rss域名
+  SEO_description: 'XiZhang-Truth',
+  SEO_keywords: 'XiZhang-Truth',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc", // cloudflare的webanalyze
 }
